526a4cf01722dec4ccea52580a91bf044449b8e8
albert752 <azemar.albert@gmail.com>
2020-04-05 19:14:33 +0200

    fix install

--- installer/install.sh
+++ installer/install.sh
@@ -2,18 +2,18 @@
 
 # Update on the system
 echo "****** PERFORMING SYSTEM UPGRADE STEP 1/7 ******"
-apt update -y
-apt upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
 
 echo "****** INSTALLING COMPONENTS STEP 2/7 ******"
 # Install the required components
-apt install git -y
-apt install python -y 
-apt install python3-pip -y
-apt install apache2 -y
-apt install libapache2-mod-wsgi-py3 python-dev python3-dev -y
-a2enmod wsgi
-apt install rpi.gpio -y
+sudo apt install git -y
+sudo apt install python -y 
+sudo apt install python3-pip -y
+sudo apt install apache2 -y
+sudo apt install libapache2-mod-wsgi-py3 python-dev python3-dev -y
+sudo a2enmod wsgi
+sudo apt install rpi.gpio -y
 
 echo "****** CLONING REPO STEP 3/7 ******"
 # Clone the repository
@@ -21,7 +21,7 @@ git clone https://github.com/albert752/RespiratorControlSystem.git
 
 echo "****** COPY OF THE FILES STEP 4/7 ******"
 # Copy the WebServer files to the correct location
-cp -r RespiratorControlSystem/WebServer /var/www
+sudo cp -r RespiratorControlSystem/WebServer /var/www
 
 echo "****** INSTALLING FLASK STEP 5/7 ******"
 # Install flask
@@ -30,15 +30,15 @@ pip3 install rpi.gpio
 
 echo "****** APACHE2 CONFIGURATION STEP 6/7 ******"
 # Copy the apache2 configuration
-cp configs/webserver.conf /etc/apache2/sites-available
-a2ensite webserver
-a2dissite 000-default.conf 
+sudo cp configs/webserver.conf /etc/apache2/sites-available
+sudo a2ensite webserver
+sudo a2dissite 000-default.conf 
 
 echo "****** START SERVICE STEP 7/7 ******"
 # Restart apache2
-service apache2 stop
+sudo service apache2 stop
 sleep 1
-service apache2 start
+sudo service apache2 start
 
 
 
